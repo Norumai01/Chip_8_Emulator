afddcbd409cdb60150d343c20a4f49b74671d739
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-04-01 00:34:32 -0400

    Made SDL2 work. Use make and main from terminal to work.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main(int argc, char** argv) {
+const int WIDTH = 1000, HEIGHT = 1000;
+
+int main(int argc, char* argv[]) {
     
-    std::cout << "Hello World" << std::endl;
-    
-    return 0;
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (NULL == window)
+    {
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Event event;
+
+    while(true) 
+    {
+        if (SDL_PollEvent(&event))
+        {
+            if (SDL_QUIT == event.type ) { break; }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
